934be4701458c620117b681e802e4511e5984551
Toby Chapman <tchap9@hotmail.com>
2025-12-21 14:17:24 +1000

    feat: added a tuning system for the hood

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoodTuning.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.DashboardCore;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class HoodTuning extends OpMode {
+
+    DcMotorEx leftShooterMotor;
+    DcMotorEx rightShooterMotor;
+    Servo hoodServo;
+
+    @Override
+    public void init() {
+        this.hoodServo = hardwareMap.get(Servo.class, "hoodServo");
+        this.leftShooterMotor = hardwareMap.get(DcMotorEx.class, "leftShooterMotor");
+        this.rightShooterMotor = hardwareMap.get(DcMotorEx.class, "rightShooterMotor");
+        this.rightShooterMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Config
+    public static class MotorPositions {
+        public static double ShooterPower = 0;
+        public static double hoodServoPosition = 0;
+
+    }
+
+    @Override
+    public void loop() {
+        this.hoodServo.setPosition(MotorPositions.hoodServoPosition);
+        this.leftShooterMotor.setPower(MotorPositions.ShooterPower);
+        this.rightShooterMotor.setPower(MotorPositions.ShooterPower);
+        telemetry.addData("Shooter Velocity", leftShooterMotor.getVelocity());
+        telemetry.update();
+
+    }
+
+}
