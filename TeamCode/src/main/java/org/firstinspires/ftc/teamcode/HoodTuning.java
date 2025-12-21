9bfe27d9447d0067c26a409ac02326d60d5f478c
Toby Chapman <tchap9@hotmail.com>
2025-12-21 14:54:03 +1000

    feat: made HoodTuning OpMode and fixed bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoodTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoodTuning.java
@@ -4,10 +4,12 @@ import com.acmerobotics.dashboard.DashboardCore;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp
 public class HoodTuning extends OpMode {
 
     DcMotorEx leftShooterMotor;
@@ -19,7 +21,7 @@ public class HoodTuning extends OpMode {
         this.hoodServo = hardwareMap.get(Servo.class, "hoodServo");
         this.leftShooterMotor = hardwareMap.get(DcMotorEx.class, "leftShooterMotor");
         this.rightShooterMotor = hardwareMap.get(DcMotorEx.class, "rightShooterMotor");
-        this.rightShooterMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.leftShooterMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Config
