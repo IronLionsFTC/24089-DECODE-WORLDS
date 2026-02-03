7570eac46b310c31c5252d92a070bef71e0a8a87
hchap1 <hchap1@hotmail.com>
2026-02-03 19:37:48 +1000

    fix: use analog input class over higher level analog sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lioncore/hardware/AbsoluteEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lioncore/hardware/AbsoluteEncoder.java
@@ -1,18 +1,18 @@
 package org.firstinspires.ftc.teamcode.lioncore.hardware;
 
-import com.qualcomm.robotcore.hardware.AnalogSensor;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class AbsoluteEncoder {
-    private AnalogSensor analogSensor;
+    private AnalogInput analogSensor;
     private double position;
 
     public AbsoluteEncoder(HardwareMap hardwareMap, String name, double zeroPosition) {
-        this.analogSensor = hardwareMap.get(AnalogSensor.class, name);
+        this.analogSensor = hardwareMap.get(AnalogInput.class, name);
     }
 
     public void read() {
-        this.position = analogSensor.readRawVoltage();
+        this.position = analogSensor.getVoltage();
     }
 
     public double position() {
