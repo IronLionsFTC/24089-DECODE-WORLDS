2637476ff05348d746ad75f4d04c3098491fc3e8
hchap1 <hchap1@hotmail.com>
2026-02-04 12:35:31 +1000

    fix: fixed sign error on polarisation that caused loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parameters/Zeroing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parameters/Zeroing.java
@@ -22,14 +22,14 @@ public class Zeroing {
 
     public static double podAngle(double voltage, double zeroPosition) {
         double unwrapped = (voltage - zeroPosition) * Zeroing.Constants.scale;
-        while (unwrapped < 180) unwrapped += 360;
+        while (unwrapped < -180) unwrapped += 360;
         while (unwrapped > 180) unwrapped -= 360;
         return unwrapped;
     }
 
     public static double polarQuadrature(double rawTicks) {
         double degrees = rawTicks * (360.0 / 4096.0);
-        while (degrees < 180) degrees += 360;
+        while (degrees < -180) degrees += 360;
         while (degrees > 180) degrees += 360;
         return degrees;
     }
