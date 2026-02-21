589608393604d672c91c4fc206bc599a80c35a5b
Harrison Chapman <hchap1@hotmail.com>
2026-02-21 18:55:21 +1000

    feat: regressions for hood angle and velocity

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/projectileMotion/Regressions.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.projectileMotion;
+
+public class Regressions {
+    public static class Velocity {
+        public static double a = 1.69502;
+    }
+
+    public static class Angle {
+        public static double a = 1.30326;
+        public static double c = -17.15683;
+    }
+
+    public static double rpmToVelocity(double rpm) {
+        return rpm * Velocity.a;
+    }
+
+    public static double velocityToRpm(double velocity) {
+        return velocity / Velocity.a;
+    }
+
+    public static double hoodAngleToLaunchAngle(double hoodAngle) {
+        return hoodAngle * Angle.a + Angle.c;
+    }
+
+    public static double launchAngleToHoodAngle(double launchAngle) {
+        return (launchAngle - Angle.c) / Angle.a;
+    }
+}
