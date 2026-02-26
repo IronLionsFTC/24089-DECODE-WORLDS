3f95021f6615b04fcc2501cce4b1cc9be38154c3
Harrison Chapman <hchap1@hotmail.com>
2026-02-27 05:05:08 +1000

    feat: new regressions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/projectileMotion/Regressions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/projectileMotion/Regressions.java
@@ -1,28 +1,28 @@
 package org.firstinspires.ftc.teamcode.projectileMotion;
 
+import com.acmerobotics.dashboard.config.Config;
+
 public class Regressions {
     public static class Velocity {
-        public static double a = 1.69502;
+        public static double a = -0.000191849;
+        public static double b = 2.57433;
     }
 
+    @Config
     public static class Angle {
-        public static double a = 1.30326;
-        public static double c = -17.15683;
+        public static double a = 1.16209;
+        public static double c = -5.64129;
     }
 
     public static double rpmToVelocity(double rpm) {
-        return rpm * Velocity.a;
+        return rpm * rpm * Velocity.a + rpm * Velocity.b;
     }
 
     public static double velocityToRpm(double velocity) {
-        return velocity / Velocity.a;
-    }
-
-    public static double hoodAngleToLaunchAngle(double hoodAngle) {
-        return hoodAngle * Angle.a + Angle.c;
+        return (-Velocity.b + Math.sqrt(Velocity.b * Velocity.b + 4 * Velocity.a * velocity)) / (2 * Velocity.a);
     }
 
     public static double launchAngleToHoodAngle(double launchAngle) {
-        return launchAngle * 1.2;//return (launchAngle - Angle.c) / Angle.a;
+        return (launchAngle - Angle.c) / Angle.a;
     }
 }
