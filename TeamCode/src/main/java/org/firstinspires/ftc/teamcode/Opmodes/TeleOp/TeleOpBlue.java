fd3986b00b9ffd86d242ab87d45558726e7160cc
Harrison Chapman <hchap1@hotmail.com>
2026-03-28 10:12:28 +1000

    fix: zero on the opposite side of the wall

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/TeleOp/TeleOpBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/TeleOp/TeleOpBlue.java
@@ -19,7 +19,7 @@ public class TeleOpBlue extends TaskOpMode {
     public Jobs spawn() {
 
         SwerveDrive drivetrain = new SwerveDrive(
-            new Position(3500, 0, 0),
+            new Position(3500, 3100, 0),
             controller1.rightJoystick::x,
             true
         );
